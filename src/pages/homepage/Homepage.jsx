7723751f53f9888c7991349d34c56bdb1ffc75ce
AMMesic <54796060+AMMesic@users.noreply.github.com>
2020-01-06 01:56:55 +0100

    Add component and pages directories and broken down the homepage into compontens. Add styling to homepage

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Directory  from '../../components/directory/Directory'
+
+import './Homepage.scss'
+
+const Homepage = () => {
+    return (
+      <div className='Homepage'>
+        <Directory />
+      </div>
+    );
+}
+
+export default Homepage
