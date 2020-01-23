964aff6c4376cf2ea825cc61620b702c0d4b0754
AMMesic <54796060+AMMesic@users.noreply.github.com>
2020-01-23 18:38:18 +0100

    Implement firebase utils. Also authenticated user into firestore database.

--- src/pages/sign-in-and-sign-up/SignInAndSignUp.jsx
+++ src/pages/sign-in-and-sign-up/SignInAndSignUp.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import './SignInAndSignUp.scss'
+import React from 'react';
+import './SignInAndSignUp.scss';
 
-import SignIn from "../../components/sign-in/SignIn";
+import SignIn from '../../components/sign-in/SignIn';
+import SignUp from '../../components/sign-up/SignUp';
 
 const SignInAndSignUp = () => {
-    return (
-        <div className="sign-in-and-sign-up">
-            <SignIn />
-        </div>
-    )
-}
+  return (
+    <div className='sign-in-and-sign-up'>
+      <SignIn />
+      <SignUp />
+    </div>
+  );
+};
 
-export default SignInAndSignUp
-
+export default SignInAndSignUp;
